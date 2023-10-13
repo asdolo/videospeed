38f31ac0b1f1d0801324d56a910b474921d54b8e
Lucas Raposeiras <raposeiras.lucas@gmail.com>
2023-10-12 22:26:19 -0300

    Fix speed controller not showing in Twitch

--- inject.js
+++ inject.js
@@ -246,9 +246,9 @@ function defineVideoController() {
           log("mutation of A/V element", 5);
           var controller = this.div;
           if (!mutation.target.src && !mutation.target.currentSrc) {
-            controller.classList.add("vsc-nosource");
+            // controller.classList.add("vsc-nosource");
           } else {
-            controller.classList.remove("vsc-nosource");
+            // controller.classList.remove("vsc-nosource");
           }
         }
       });
@@ -282,7 +282,7 @@ function defineVideoController() {
     wrapper.classList.add("vsc-controller");
 
     if (!this.video.src && !this.video.currentSrc) {
-      wrapper.classList.add("vsc-nosource");
+      // wrapper.classList.add("vsc-nosource");
     }
 
     if (tc.settings.startHidden) {
